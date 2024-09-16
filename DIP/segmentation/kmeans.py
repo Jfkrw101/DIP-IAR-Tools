1f07f3c159319ea3a60fd7c9d5b7e26a7af40d20
Jfkrw101 <krairawee.r@gmail.com>
2024-09-16 10:51:44 +0700

    fix syntax error

--- DIP/segmentation/kmeans.py
+++ DIP/segmentation/kmeans.py
@@ -10,8 +10,7 @@ def kmeans(input_img, k):
     input_rec = input_rec.astype(np.float32)
 
     ### -> K-means Clustering
-    criteria = (cv2.TERM_CRITERIA_MAX_ITER +
-    cv2.TERM_CRITERIA_EPS, 100, 0.2) \
+    criteria = (cv2.TERM_CRITERIA_MAX_ITER + cv2.TERM_CRITERIA_EPS, 100, 0.2)
     _, label_rec, (centers) = cv2.kmeans(input_rec,k, None, criteria, 10,cv2.KMEANS_RANDOM_CENTERS)
     # -> Assign Label with Center Color
     centers = centers.astype(np.uint8)
