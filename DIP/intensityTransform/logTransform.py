07a0d88fa75564ad0648f290c4d06649e3f86647
Jfkrw101 <krairawee.r@gmail.com>
2024-09-16 10:51:19 +0700

    fix syntax error

--- DIP/intensityTransform/logTransform.py
+++ DIP/intensityTransform/logTransform.py
@@ -3,7 +3,7 @@ import numpy as np
 def logTransform(input_array, c=None, to_uint8=True):
     # -> Default C
     if c == None:
-        max_level = np.iinfo(input_array.dtype).max – np.iinfo(input_array.dtype).min
+        max_level = np.iinfo(input_array.dtype).max - np.iinfo(input_array.dtype).min
         c = max_level / (np.log(1 + np.max(input_array)))
     # -> Convert "input_array", prevent overflow
     input_array = input_array.astype(float)
